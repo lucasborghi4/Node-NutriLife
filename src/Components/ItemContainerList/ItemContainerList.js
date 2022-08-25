c823913990e037f0f88e8dc7bf8dc4884a57d567
Lucas Borghi <lucas.borghi@ing.austral.edu.ar>
2022-08-25 19:20:34 -0300

    carrito logo y tarjetas

--- src/Components/ItemContainerList/ItemContainerList.js
+++ src/Components/ItemContainerList/ItemContainerList.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import "./ItemContainerList.css";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
-export const ItemContainerList = () => {
-  return (
-    <div>Aca vas a encontrar todas los productos de NutriLife</div>
-  )
-}
+export default function MediaCard(props) {
+    return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={props.imagen}
+          alt={props.elemento}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          {props.elemento}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          {props.descripcion}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
